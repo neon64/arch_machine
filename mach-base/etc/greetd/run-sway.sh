8b7e99e612b2cbc71b63d6b7ffd29c22c12ab7da
Chris Chamberlain <chris@chamberlain.id.au>
2021-09-16 21:03:36 +1000

    autologin to "Login" keychain through greetd

--- mach-base/etc/greetd/run-sway.sh
+++ mach-base/etc/greetd/run-sway.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+# https://wiki.archlinux.org/title/GNOME/Keyring
+# need to complete the gnome keyring setup here
+eval $(gnome-keyring-daemon --start)
+export SSH_AUTH_SOCK
+
 env _JAVA_AWT_WM_NONREPARENTING=1 MOZ_ENABLE_WAYLAND=1 XDG_SESSION_TYPE=wayland XDG_CURRENT_DESKTOP=sway XDG_SESSION_DESKTOP=sway systemd-cat -t sway sway
